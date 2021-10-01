9b7bd8566d735e429133ce2a2bcb2841f78be7ea
notsofunctional <jorge@gendra.dev>
2021-10-01 14:50:04 -0500

    chore: adaptations to current chachanges from pigeon and federated plugin

--- packages/pusher_beams_platform_interface/test/pusher_beams_platform_interface_test.dart
+++ packages/pusher_beams_platform_interface/test/pusher_beams_platform_interface_test.dart
@@ -4,9 +4,6 @@ import 'package:pusher_beams_platform_interface/pusher_beams_platform_interface.
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+
   });
 }
