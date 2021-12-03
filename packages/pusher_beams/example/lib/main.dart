394fc9c6ab9c2cd7b238191f79ad5b51bff4f427
notapineapple <damnyorch@gmail.com>
2021-12-03 02:20:05 -0600

    chore: example for three platforms (unfinished)

--- packages/pusher_beams/example/lib/main.dart
+++ packages/pusher_beams/example/lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:pusher_beams/pusher_beams.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  await PusherBeams.instance.start('fe8d1a3b-49f8-4375-a278-a49e6bb7f020');
 }
 
 class MyApp extends StatefulWidget {
@@ -16,34 +18,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
 
   @override
-  void initState() {
+  initState() {
     super.initState();
-    initPlatformState();
+
+    testInterestsChanges();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await PusherBeams.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+  testInterestsChanges() async {
+    if (!kIsWeb) {
+      await PusherBeams.instance.onInterestChanges((interests) => {
+        print('Interests change: $interests')
+      });
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -54,7 +42,13 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: OutlinedButton(
+            onPressed: () async {
+              await PusherBeams.instance.clearDeviceInterests();
+              await PusherBeams.instance.addDeviceInterest('debug0-hello');
+            },
+            child: const Text('START'),
+          ),
         ),
       ),
     );
